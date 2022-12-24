63e0b0f5af3cbb6b204855e85a83279910d6d7ad
Natalya <ne.emelkina@gmail.com>
2022-12-24 17:27:38 +0300

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if (N<1000 && N>99)
+{
+Console.Write(N = N%10); 
+}
+else if (N>999)
+{
+    while (N>999)
+    {
+        N = N/10;  
+    }
+    Console.Write(N = N%10); 
+}
+else 
+{
+Console.Write("У введённого числа нет 3 цифры");
+}
